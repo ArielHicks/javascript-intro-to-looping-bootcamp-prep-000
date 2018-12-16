b091943965ba3619f009b72156ba79bc6f41c681
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-16 22:17:42 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+//for ([initialization]; [condition]; [iteration]) {
+//[loopBody];
+//}
+
+function forLoop(array){
+  for (let i = 1; i < 25; i++){
+    if (i == 1){
+    array.push(`I am 1 strange loop`)
+}
+    else {
+    array.push(`I am ${i} strange loop.`)
+  }
+    return array
+}
+}
