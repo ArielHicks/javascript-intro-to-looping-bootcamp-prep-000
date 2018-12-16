67d2d99de35a6a176ad987a6945fbbd5facd45f0
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-16 22:20:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@
 function forLoop(array){
   for (let i = 1; i < 26; i++){
     if (i == 1){
-    array.push(`I am 1 strange loop`)
+    array.push(`I am 1 strange loop.`)
 }
     else {
     array.push(`I am ${i} strange loop.`)
